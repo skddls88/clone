0fd619db24a11ed7eb457948b7d2e3f11b85706f
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-11-16 21:39:59 +0900

    5.3

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,9 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
