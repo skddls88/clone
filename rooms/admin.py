2e8899b08f24b8271bb2c29252ffefef34cefeb8
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-11-18 00:56:32 +0900

    6.2

--- rooms/admin.py
+++ rooms/admin.py
@@ -51,6 +51,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
 
     list_filter = (
@@ -64,6 +65,12 @@ class RoomAdmin(admin.ModelAdmin):
         "country",
     )
 
+    # ordering = (
+    #     "name",
+    #     "price",
+    #     "bedrooms",
+    # )
+
     search_fields = ("=city", "^host__username")
 
     filter_horizontal = (
@@ -71,6 +78,12 @@ class RoomAdmin(admin.ModelAdmin):
         "facilities",
         "house_rules",
     )
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Potato"
+
+    count_amenities.short_description = "hello sexy!"
 
 
 @admin.register(models.Photo)
