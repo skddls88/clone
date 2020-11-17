9d8c732d777378e7a4a8d1ce4eac56355a280a0a
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-11-18 00:46:23 +0900

    6.1

--- rooms/models.py
+++ rooms/models.py
@@ -66,8 +66,8 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
     room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenty = models.ManyToManyField(Amenity, blank=True)
-    Facilities = models.ManyToManyField(Facility, blank=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
     house_rules = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self):
