c7b1f82d68fd8d3d6a42514916b7d506a225ab07
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-11-28 15:13:13 +0900

    9.3 seed room 생성

--- rooms/models.py
+++ rooms/models.py
@@ -84,9 +84,11 @@ class Room(core_models.TimeStampedModel):
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_rating = 0
-        for review in all_reviews:
-            all_rating += review.rating_average()
-        return all_rating / len(all_reviews)
+        if len(all_reviews) > 0:
+            for review in all_reviews:
+                all_rating += review.rating_average()
+            return all_rating / len(all_reviews)
+        return 0
 
 
 class Photo(core_models.TimeStampedModel):
