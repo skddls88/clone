15e54e994c48f015663128f915b0da8dd650e8af
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-02 22:20:24 +0900

    12.4

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.http import Http404
 from django.shortcuts import render
 from . import models
@@ -15,9 +15,8 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-    except models.Room.DoesNotExist:
-        raise Http404()
-    return render(request, "rooms/detail.html", {"room": room})
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
