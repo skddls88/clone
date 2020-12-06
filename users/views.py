5a16a3c45e0450398cc8d65a3f755d7db7edaf8f
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-06 15:57:49 +0900

    15.0

--- users/views.py
+++ users/views.py
@@ -40,3 +40,15 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Park",
+        "last_name": "KeumSeo",
+        "email": "qkrrmatj88@naver.com",
+    }
