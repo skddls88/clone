b259297da6803c66a059d5745e42515576039a8c
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-06 14:06:23 +0900

    14.2

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    # 확인하고 싶은 필드가 있으면 앞에 clean 을 써야한다.
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        print("clean password")
