54931b4ee54d4588d402017e3493a1526164aae5
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-06 14:25:35 +0900

    14.3

--- users/forms.py
+++ users/forms.py
@@ -8,13 +8,15 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
     # 확인하고 싶은 필드가 있으면 앞에 clean 을 써야한다.
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
 
-    def clean_password(self):
-        print("clean password")
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return password
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
+        except models.User.DoesNotExist:
+            self.adderror("email", forms.ValidationError("User does not exist"))
