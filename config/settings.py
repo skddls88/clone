35a3e959b79572acc2a4ec601dd78fd35171caf5
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-09 21:09:42 +0900

    16.1 django-dotenv 및 mailgun.com 회원가입

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,10 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAIL_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAIL_PASSWORD")
