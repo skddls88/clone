54eb0580cf3c1b5e04215a728f7e596f30711326
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-09 20:36:32 +0900

    15.2

--- users/models.py
+++ users/models.py
@@ -30,8 +30,12 @@ class User(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
     birthday = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRNECY_KRW
+    )
     superhost = models.BooleanField(default=False)
 
     def __str__(self):
