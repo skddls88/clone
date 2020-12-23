54f9316094a3bc8cdd18bab0c0f0dd18f9ff3b02
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-23 16:09:30 +0900

    21.3

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.mail import send_mail
+from django.shortcuts import reverse
 
 
 class User(AbstractUser):
@@ -55,6 +56,10 @@ class User(AbstractUser):
     login_method = models.CharField(
         max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
+
+    def get_absolute_url(self):
+
+        return reverse("users:profile", kwargs={"pk": self.pk})
 
     # def verify_email(self):
     #     if self.email_verified is False:
--- users/urls.py
+++ users/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("login/kakao/callback", views.kakao_callback, name="kakao-callback"),
     path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
+    path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
 ]
