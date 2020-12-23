dfea3574c01906dec090e92c122149350efc4238
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-12-23 16:42:14 +0900

    20.10

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,10 @@ from . import models
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "password"})
+    )
 
     # 확인하고 싶은 필드가 있으면 앞에 clean 을 써야한다.
 
@@ -24,5 +26,34 @@ class LoginForm(forms.Form):
 
 
 class SignUpForm(UserCreationForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email"}),
+        }
 
-    username = forms.EmailField(label="Email")
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
--- users/views.py
+++ users/views.py
@@ -52,11 +52,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "Park",
-        "last_name": "KeumSeo",
-        "email": "qkrrmatj88@naver.com",
-    }
 
     def form_valid(self, form):
         form.save()
